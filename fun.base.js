046cd1699547d19ca76e4f84bc2e0cf59b1f398a
a89194412 <42599731+a89194412@users.noreply.github.com>
2018-10-31 19:44:45 +0800

    Create fun.base.js

---
+++ fun.base.js
@@ -0,0 +1,1 @@
+function rand(minDit,maxDit){return Math.floor(Math.random()*(maxDit-minDit+1))+minDit;};function getViewSize(){var de=document.documentElement;var db=document.body;var viewW=de.clientWidth==0?db.clientWidth:de.clientWidth;var viewH=de.clientHeight==0?db.clientHeight:de.clientHeight;return Array(viewW,viewH);};
